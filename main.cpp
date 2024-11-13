9a15b27d8cd7640b877b2f12aa734dbce38214ec
Vevaan <vevaan.s.verma@gmail.com>
2024-11-13 00:30:49 -0800

    Add queue functionality

--- main.cpp
+++ main.cpp
@@ -19,6 +19,9 @@ struct Car {
 	}
 };
 
+/* FUNCTION PROTOTYPES */
+void outputQueue(deque<Car>& queue);
+
 /* CONSTANTS */
 const int INITIAL_CARS = 2;
 
@@ -27,7 +30,12 @@ const int INITIAL_CARS = 2;
 // returns: int - the exit code of the program
 int main() {
 
-	deque<Car> queue = deque<Car>(INITIAL_CARS);
+	srand(time(0)); // seed the random number generator
+
+	deque<Car> queue(INITIAL_CARS); // createa a deque of cars with 2 initial cars
+
+	cout << "Initial "; // add "Initial" as a prefix to the output
+	outputQueue(queue); // output initial queue
 
 	int time = 1; // initialize time to 0
 
@@ -36,10 +44,39 @@ int main() {
 		// generate random percentage
 		int percentage = rand() % 100 + 1;
 
+		cout << "Time: " << time << " | Operation: "; // output time and operation prefix
+
+		if (percentage <= 55) { // 55% chance that front car pays toll and is removed from the queue
+
+			Car car = queue.front(); // get the front car
+			queue.pop_front();
+
+		} else { // 45% chance of a new car being added to the back of the queue
+
+			Car newCar = Car(); // create a new car
+			queue.push_back(newCar); // add a new car to the back of the queue
+
+		}
+
+		outputQueue(queue); // output the queue
 		time++;
 
 	}
 
 	return 0;
+
+}
+
+// outputQueue() outputs the contents of the queue
+// arguments: deque<Car>& - the queue to output
+// returns: void
+void outputQueue(deque<Car>& queue) {
+
+	cout << "Queue:" << endl; // output queue header
+
+	for (Car car : queue)
+		cout << "  [" << car.year << " " << car.manufacturer << " (" << car.transponder << ")]" << endl;
+
+	cout << endl; // output blank line for formatting purposes
 
 }
