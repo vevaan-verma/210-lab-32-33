3d65d79067d6a724fc3b80ee03c17c8dee9f9b84
Vevaan <vevaan.s.verma@gmail.com>
2024-11-13 00:11:31 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// COMSC-210 | Lab 32 | Vevaan Verma
+using namespace std;
+#include <iostream>
+
+// main() is the entry point of the program and drives the program
+// arguments: none
+// returns: int - the exit code of the program
+int main() {
+
+	return 0;
+
+}
